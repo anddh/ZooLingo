a100433f2e5e10dc9f3e681090df90c86acf9ef9
anddh <foofty@gmail.com>
2026-02-08 21:27:05 +0000

    Small updates

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const ASSETS = [
   './',
   './index.html',
   './manifest.json',
+  './icon.png',
+  './icon_splash.png',
   'https://fonts.googleapis.com/css2?family=Mochiy+Pop+One&display=swap'
 ];
 
@@ -11,7 +13,5 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
